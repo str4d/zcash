810bc4383d3fc79dafbed7f7aa99b6fcf185f1d2
Cory Fields <cory-nospam-@coryfields.com>
2017-01-19 17:20:35 -0500

    depends: add a zlib build
    Zcash: Upstream added this for QT; we are adding it for Tor.

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,7 +1,7 @@
 rust_crates := crate_libc
 rust_packages := rust $(rust_crates) librustzcash
 proton_packages := proton
-zcash_packages := libgmp libsodium
+zcash_packages := libgmp libsodium zlib
 packages := boost openssl libevent zeromq $(zcash_packages) googletest
 native_packages := native_ccache
 
