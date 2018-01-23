609e74479b6f490e76d4428639401e9d3177b712
Russell Yanofsky <russ@yanofsky.org>
2018-01-23 13:16:56 -0500

    Make CKey::Load references const
    No change in behavior, this just prevents CKey::Load arguments from looking
    like outputs.

--- src/key.h
+++ src/key.h
@@ -138,7 +138,7 @@ public:
     bool VerifyPubKey(const CPubKey& vchPubKey) const;
 
     //! Load private key and check that public key matches.
-    bool Load(CPrivKey& privkey, CPubKey& vchPubKey, bool fSkipCheck);
+    bool Load(const CPrivKey& privkey, const CPubKey& vchPubKey, bool fSkipCheck);
 
     //! Check whether an element of a signature (r or s) is valid.
     static bool CheckSignatureElement(const unsigned char* vch, int len, bool half);
