4bacb8fc4702080c16df2c34c088c3d37addd0c9
Jack Grigg <jack@z.cash>
2021-03-06 18:40:34 +0000

    depends: Add cxxbridge command to dependencies

---
+++ depends/packages/native_cxxbridge.mk
@@ -0,0 +1,17 @@
+package=native_cxxbridge
+$(package)_version=1.0.49
+$(package)_download_path=https://github.com/dtolnay/cxx/archive
+$(package)_file_name=native_cxxbridge-$($(package)_version).tar.gz
+$(package)_download_file=$($(package)_version).tar.gz
+$(package)_sha256_hash=ec47c18cffaedb56f31c9631e48950ac7f39012d0a4f9cc38536e74325a5dd99
+$(package)_build_subdir=gen/cmd
+$(package)_dependencies=native_rust
+
+define $(package)_build_cmds
+  cargo build --release
+endef
+
+define $(package)_stage_cmds
+  mkdir -p "$($(package)_staging_prefix_dir)" && \
+  cargo install --path=. --root=$($(package)_staging_prefix_dir)
+endef
