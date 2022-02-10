e58b383d0d10d95178a1ce1673bd2605cd7e47c0
Kris Nuttycombe <kris@nutty.land>
2022-02-10 11:36:24 -0700

    WIP: orchard wallet tests.

---
+++ src/wallet/gtest/test_orchard_wallet.cpp
@@ -0,0 +1,30 @@
+#include "random.h"
+#include "utiltest.h"
+#include "wallet/orchard.h"
+
+#include <optional>
+
+
+TEST(OrchardWalletTests, AddSpendingKey) {
+}
+
+TEST(OrchardWalletTests, AddFullViewingKey) {
+}
+
+TEST(OrchardWalletTests, AddRawAddress) {
+}
+
+TEST(OrchardWalletTests, AddNotes) {
+}
+
+TEST(OrchardWalletTests, AppendNoteCommitments) {
+}
+
+TEST(OrchardWalletTests, CheckpointNoteCommitmentTree) {
+}
+
+TEST(OrchardWalletTests, RewindToLastCheckpoint) {
+}
+
+TEST(OrchardWalletTests, IsMine) {
+}
